4ae18d47b4b43dfecab6c52604b6b4359c7c070e
gumchik <nagumanov.mark@gmail.com>
2017-07-10 02:38:41 +0500

    Ignor

--- Task141/Task141/Program.cs
+++ Task141/Task141/Program.cs
@@ -10,18 +10,25 @@ namespace Task141
     {
         static void Main(string[] args)
         {
+            //string[] sNumSys = {"BIN", "TRI", "OCT", "DEC", "HEX" };
             Console.Write("Введите число: ");
             int iNum = int.Parse(Console.ReadLine());
             Console.Write("Введите систему счисления из следующих: " +
-                "2 - двоичная, 8 - восьмеричная, 10 - десятичная, 16 - шестнадцатиричная:/n");
-            int iNumSys = int.Parse(Console.ReadLine());
-            switch(iNumSys)
+                "BIN - двоичная, TRI - троичная, OCT - восьмеричная, DEC - десятичная, HEX - шестнадцатиричная:/n");
+            string sNumSys = Console.ReadLine();
+            switch(sNumSys)
             {
-                case '2':
+                case "BIN":
 
                     break;
-                case '8':
+                case "TRI":
                     Console.Write("2");
+                    break;
+                case "OCT":
+                    break;
+                case "DEC":
+                    break;
+                case "HEX":
                     break;
                 default:
                     break;
